a7fb82ee8845ac77505d82b4b5eb3acdf13d17fe
FaithIsWork11 <faithiswork@duck.com>
2024-07-09 19:35:45 +0000

    logo

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 const Navbar = () => {
 return (
-<nav className="navbar navbar-expand-lg navbar-light bg-dark ">
+<nav className="navbar navbar-expand-lg navbar-light bg-dark  p-3">
   <div className="container-fluid ">
-    <a className="navbar-brand text-light" href="#">Navbar</a>
+    <a className="navbar-brand text-light" href="#">Bootstrap</a>
     <button className="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
